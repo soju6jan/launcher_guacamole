265ecfebeb1305bcb3a2015a0186c9d0188c41cc
soju6jan <cybersol@naver.com>
2020-10-07 15:18:14 +0900

    1

--- guacamole/install.sh
+++ guacamole/install.sh
@@ -2,8 +2,8 @@
 apk add --no-cache openjdk8
 #wget http://apache.tt.co.kr/tomcat/tomcat-8/v8.5.50/bin/apache-tomcat-8.5.50.tar.gz
 wget http://apache.tt.co.kr/tomcat/tomcat-8/v8.5.58/bin/apache-tomcat-8.5.58.tar.gz
-tar xvzf apache-tomcat-8.5.57.tar.gz
-mv apache-tomcat-8.5.57 /var/lib/tomcat
+tar xvzf apache-tomcat-8.5.58.tar.gz
+mv apache-tomcat-8.5.58 /var/lib/tomcat
 cp -R /app/data/custom/launcher_guacamole/guacamole /etc/guacamole/
 ln -s /etc/guacamole/guacamole.war /var/lib/tomcat/webapps/
 export GUACAMOLE_HOME=/etc/guacamole
